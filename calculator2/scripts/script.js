527fd535f2a70f3e3de69fef8b3dd153c83df273
Dominik Chally <dominik.chally@yahoo.com>
2022-01-12 19:26:04 -0600

    Added a todo list and then continued working on basic calculator

--- calculator2/scripts/script.js
+++ calculator2/scripts/script.js
@@ -2,14 +2,31 @@ let buttons = document.querySelector('.button');
 
 buttons.addEventListener('click', (event) => {
     const elem = event.target;
+    let calcValue = document.querySelector('.Calc-input');
+    
 
-    if (elem.id === 'double') {
-        document.querySelector('.Calc-input').value *= 2;
-    } else if(elem.id === 'fourth'){
-        document.querySelector('.Calc-input').value /= 4;
-
-    }
-
-
-
+    if (elem.id === 'add') {
+        calcValue.value = 0;
+    } else if (elem.id === 'fourth') {
+        calcValue.value /= 4;
+    } else if (elem.id === 'clear') {
+        calcValue.value = 0;
+    } else if (elem.id === 'decimal') {
+        if (!calcValue.value.includes('.')) {
+            calcValue.value += '.';
+        }
+    } else if (elem.id ==='neg'){
+        if (!calcValue.value.includes('-') && (calcValue.value !== '0')){
+            calcValue.value = '-' + calcValue.value;
+        } else{
+            calcValue.value = calcValue.value.slice(1);
+        }
+    } else if (elem.className === 'number-button') {
+        let userNum = elem.innerHTML;
+        if (calcValue.value === "0") {
+            calcValue.value = userNum;
+        } else {
+            calcValue.value += userNum;
+        }
+    } 
 });
